accf071bef679c447a31d0a191227197bec045b2
Diederick Lawson <diederick@altovista.nl>
2024-10-24 10:21:34 +0200

    Bump back to 0.9.0

--- mix.exs
+++ mix.exs
@@ -16,7 +16,7 @@ defmodule WuunderUtils.MixProject do
       start_permanent: Mix.env() == :prod,
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [coveralls: :test, "coveralls.html": :test],
-      version: "0.9.0-rc.0"
+      version: "0.9.0"
     ]
   end
 
