82ad1666733b2987394e1cca8b19ebfa90071737
Diederick Lawson <diederick@altovista.nl>
2024-10-24 09:45:37 +0200

    Fix dialyzer errors and upgrade hex packages

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,2 +1,3 @@
 [{"lib/wuunder_utils/maps.ex", :contract_supertype},
-{"lib/wuunder_utils/strings.ex", :contract_supertype},]
+{"lib/wuunder_utils/strings.ex", :contract_supertype},
+{"lib/wuunder_utils/numbers.ex", :contract_supertype}]
